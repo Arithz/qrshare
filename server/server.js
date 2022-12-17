b31bf4194c1ca6ae75fea87e612a34beeba4103e
Harithz <67273525+Arithz@users.noreply.github.com>
2022-12-17 13:33:28 +0800

    change to host env back

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://qr-share.netlify.app" + ":" + PORT,
+    origin: HOST + ":" + PORT,
     methods: ["GET", "POST"],
   },
 });

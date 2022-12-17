3bbc4743333fd6356b49aefff75a93df215b52ce
Harithz <67273525+Arithz@users.noreply.github.com>
2022-12-17 13:29:21 +0800

    change cors url to frontend

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: HOST + ":" + PORT,
+    origin: "https://qr-share.netlify.app" + ":" + PORT,
     methods: ["GET", "POST"],
   },
 });

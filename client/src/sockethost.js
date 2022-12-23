8357c02480f0d3d36f834d8427414e9ad1ce3bfa
Harithz <67273525+Arithz@users.noreply.github.com>
2022-12-23 12:56:27 +0800

    Updatehost

--- client/src/sockethost.js
+++ client/src/sockethost.js
@@ -1,4 +1,4 @@
 import io from "socket.io-client";
-const socket = io("http://localhost:3001", { transports: ["websocket"], upgrade: false });
-// const socket = io("https://qrshare.adaptable.app/", { transports: ["websocket"], upgrade: false });
+//const socket = io("http://localhost:3001", { transports: ["websocket"], upgrade: false });
+const socket = io("https://qrshare.adaptable.app/", { transports: ["websocket"], upgrade: false });
 export default socket;

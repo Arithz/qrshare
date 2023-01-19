a529f0c37a945d955a8a8a518f5911ee3ef3c4c2
Arithz <harithzfathi2001@gmail.com>
2023-01-19 17:40:50 +0800

    fix weird bug with a href

--- client/src/pages/Chatroom.js
+++ client/src/pages/Chatroom.js
@@ -177,8 +177,9 @@ function Chatroom() {
                       <p className="float-left">{message.userInput}</p>
                     )}
                     <BiCopy
+                      data-text={message.userInput}
                       className="float-right"
-                      onClick={copyMessage()}
+                      onClick={copyMessage}
                     />
                   </div>
                 </div>
